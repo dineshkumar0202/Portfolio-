5e5158fc96fc52fcaa33687b9bfeb18872df49a5
dineshkumar0202 <aesthetic.dinesh02@gmail.com>
2024-09-16 18:46:03 +0530

    feat: Adding My Portfolio

---
+++ script.js
@@ -0,0 +1,59 @@
+ // Add this script to your existing JavaScript
+
+ document.addEventListener("DOMContentLoaded", function() {
+    var learnMoreLink = document.querySelectorAll('a[href="#learn-more"]');
+    var learnMoreSection = document.getElementById('learn-more');
+
+    learnMoreLink.forEach(function(link) {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            learnMoreSection.style.display = 'block';
+            // You can add more logic here if needed
+        });
+    });
+});
+
+
+var tablinks =document.getElementsByClassName("tab-links");
+var tabcontents =document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for (tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+
+var sidemenu=document.getElementById("sidemenu")
+
+    function openmenu(){
+        sidemenu.style.right="0";
+    }
+    function closemenu(){
+        sidemenu.style.right="-200px";
+    } 
+
+
+    const scriptURL = 'https://script.google.com/macros/s/AKfycbxJ7d_BhKJauUGVyfqddNaQi1mtO52Pu4hAT6tA3ofcMTKewiAJjq_zVq9CBECSqcLuCQ/exec'
+    const form = document.forms['submit-to-google-sheet']
+    const msg= document.getElementById("msg")
+
+    form.addEventListener('submit', e => {
+      e.preventDefault()
+      fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => {
+            msg.innerHTML="Message Sent Successfully"
+            setTimeout(function (){
+                msg.innerHTML=""
+            },5000)
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+    })
+    
